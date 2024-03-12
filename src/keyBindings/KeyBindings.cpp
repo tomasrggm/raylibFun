10f3827012c353bc513b00fce794ee5af289470e
tdmarques <tdmarques@criticalsoftware.com>
2024-03-12 12:09:54 +0000

    * Fix for inconsistent grid alignment * Add restrictions for grid movement

--- src/keyBindings/KeyBindings.cpp
+++ src/keyBindings/KeyBindings.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+const int screenWidth = 800;
+const int screenHeight = 440;
+
 void processKeys(Unit *unit){
-    if (IsKeyPressed(KEY_RIGHT)) unit->updateX(32.0f);
-    else if (IsKeyPressed(KEY_LEFT)) unit->updateX(-32.0f);
-    else if (IsKeyPressed(KEY_UP)) unit->updateY(-32.0f);
-    else if (IsKeyPressed(KEY_DOWN)) unit->updateY(32.0f);
+    if (IsKeyPressed(KEY_RIGHT) && unit->getPlayerX() <= (screenWidth - 96.0f)) unit->updateX(32.0f);
+    else if (IsKeyPressed(KEY_LEFT) && unit->getPlayerX() >= 64.0f) unit->updateX(-32.0f);
+    else if (IsKeyPressed(KEY_UP) && unit->getPlayerY() >= 64.0f) unit->updateY(-32.0f);
+    else if (IsKeyPressed(KEY_DOWN) && unit->getPlayerY() <= (screenHeight - 64.0f)) unit->updateY(32.0f);
 }
--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -6,9 +6,9 @@
 
 int main(void) {
     const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenHeight = 448;
     InitWindow(screenWidth, screenHeight, "Punpun's game");
-    Unit player = Unit(0, 0, 32, 32);
+    Unit player = Unit(32, 32, 32, 32);
     GameCamera camera = GameCamera(1.0f, {screenWidth/2.0f, screenHeight/2.0f}, {screenWidth/2.0f, screenHeight/2.0f}, 0.0f);
     SetTargetFPS(60);
 
@@ -21,12 +21,12 @@ int main(void) {
         BeginMode2D(camera.getCamera());
         DrawRectangleRec(player.getPlayer(), RED);
         
-        for(int i = 0; i < screenWidth/32 - 1; i++){
-            DrawLineV((Vector2) { (float)32.0f * i + 32.0f, 32.0f }, (Vector2){ (float) 32.0f * i + 32.0f, (float) screenHeight - 32.0f }, LIGHTGRAY );
+        for(int i = 1; i < screenWidth/32; i++){
+            DrawLineV((Vector2) { (float)32.0f * i, 32.0f }, (Vector2){ (float) 32.0f * i, (float) screenHeight - 32.0f }, LIGHTGRAY );
         }
 
-        for(int i = 0; i < screenHeight/32 - 1; i++){
-            DrawLineV((Vector2) { 32.0f, (float)32.0f * i + 32.0f }, (Vector2){ (float) screenWidth - 32.0f ,  (float) 32.0f * i + 32.0f}, LIGHTGRAY );
+        for(int i = 1; i < screenHeight/32; i++){
+            DrawLineV((Vector2) { 32.0f, (float)32.0f * i }, (Vector2){ (float) screenWidth - 32.0f ,  (float) 32.0f * i }, LIGHTGRAY );
         }
 
 
